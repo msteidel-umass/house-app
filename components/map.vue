7444af92c02de110486d252793c8de7b16c59fa9
Michael Steidel <98567922+msteidel-umass@users.noreply.github.com>
2023-03-07 22:28:44 -0500

    Several improvements made to graph rendering

--- components/map.vue
+++ components/map.vue
@@ -988,52 +988,63 @@ export default defineComponent({
 
 		function drawGraph(geo_id) {
 			// set the dimensions and margins of the graph
-			var width = document.getElementById("graph").clientWidth;
-			var height = document.getElementById("graph").clientHeight;
+			var width = document.getElementById("graph").clientWidth - 25;
+			var height = document.getElementById("graph").clientHeight - 30; // Decreased height by 30 to prevent the graph from getting cut off
 
-			console.log(height);
-			console.log(width);
-			
-			var margin = {top: 10, right: 30, bottom: 30, left: 60};
-			//width = width - margin.left - margin.right,
-			//height = height - margin.top - margin.bottom;
-		
+			var margin = { top: 10, right: 40, bottom: 20, left: 75 }; // Decreased bottom margin to fit more space for the graph
 
 			// append the svg object to the body of the page
-			var svg = d3.select("#graph")
+			var svg = d3
+				.select("#graph")
 				.append("svg")
 				.attr("width", width)
 				.attr("height", height)
-				.attr("viewBox", "0 0 $${width + margin.left + margin.right} ${height + margin.top + margin.bottom}")
+				.attr(
+					"viewBox",
+					`0 0 ${width + margin.left + margin.right} ${height + margin.top + margin.bottom}`
+				)
 				.style("background-color", "white")
 				.append("g")
-				.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+				.attr("transform", `translate(${margin.left}, ${margin.top})`);
 
 			//Read the data
-			d3.csv(HOME_VALS).then(function(data) {
-
+			d3.csv(HOME_VALS).then(function (data) {
 				// format variables
-				data.forEach(function(d) {
+				data.forEach(function (d) {
 					d.date = d3.timeParse("%Y")(d["Year"]);
 					d.value = +d[geo_id];
 				});
 
 				// Add X axis --> it is a date format
 				var x = d3.scaleTime()
-					.domain(d3.extent(data, function(d) { return d.date; }))
-					.range([ 0, width ]);
-			
+					.domain(d3.extent(data, function (d) {
+						return d.date;
+					}))
+					.range([0, width]);
+
 				svg.append("g")
-					.attr("transform", "translate(0," + height + ")")
-					.call(d3.axisBottom(x));
+					.attr("transform", `translate(0, ${height})`)
+					.call(d3.axisBottom(x))
+					.selectAll("text")
+					.style("color", "black");
+
+				
+				var yMinValue = d3.min(data.map(function (d) { return d.value; }));
+				var yMaxValue = d3.max(data.map(function (d) { return d.value; }));
+				var yRange = yMaxValue - yMinValue;
 
 				// Add Y axis
 				var y = d3.scaleLinear()
-					.domain([0, d3.max(data, function(d) { return d.value; })])
-					.range([ height, 0 ]);
-				
+					.domain([yMinValue - yRange * 0.05, yMaxValue + yRange * 0.05])
+					.range([height, 0]);
+
 				svg.append("g")
-					.call(d3.axisLeft(y));
+					.call(d3.axisLeft(y))
+					.selectAll("text")
+					.style("color", "black");
+
+				svg.selectAll('.domain')
+					.attr('stroke', 'black');
 
 				// Add the line
 				svg.append("path")
@@ -1042,9 +1053,14 @@ export default defineComponent({
 					.attr("stroke", "steelblue")
 					.attr("stroke-width", 1.5)
 					.attr("d", d3.line()
-						.x(function(d) { return x(d.date) })
-						.y(function(d) { return y(d.value) })
-					)
+						.x(function (d) {
+							return x(d.date);
+						})
+						.y(function (d) {
+							return y(d.value);
+						})
+						.curve(d3.curveMonotoneX)
+					);
 			});
 		}
 
